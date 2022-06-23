7f8bcc790384c6603ab3a11661662aa911ca353f
Patrick Sylantys <SYLANTYS>
2022-06-23 15:30:13 -0400

    quick fix

--- tutorial/src/components/Card.js
+++ tutorial/src/components/Card.js
@@ -1,5 +1,3 @@
-import Star from "../images/star.png"
-
 export default function Card(props) {
   let badgeText
   if(props.openSpots === 0) {
@@ -13,7 +11,7 @@ export default function Card(props) {
       {badgeText && <div className="card--badge">{badgeText}</div>}
       <img src={`../images/${props.coverImg}`} className="card--image" />
       <div className="card--stats">
-        <img src={Star} className="card--star" />
+        <img src="../images/star.png" className="card--star" />
         <span>{props.stats.rating}</span>
         <span className="gray">({props.stats.reviewCount}) • </span>
         <span className="gray">{props.location}</span>
--- tutorial/src/components/Navbar.js
+++ tutorial/src/components/Navbar.js
@@ -1,9 +1,8 @@
-import NavLogo from "../images/airbnb-logo.png"
 
 export default function Navbar() {
   return (
     <nav>
-      <img src={NavLogo} className="nav--logo" />
+      <img src="../images/airbnb-logo.png" className="nav--logo" />
     </nav>
   )
 }
