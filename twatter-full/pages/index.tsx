e2189e2b6819f14e83cc6041114e5b1d93add9a5
Patrick Sylantys <SYLANTYS>
2022-07-27 20:00:27 -0400

    first commit nextjs

---
+++ twatter-full/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Twitter 2.0</title>
+      </Head>
+
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default Home
