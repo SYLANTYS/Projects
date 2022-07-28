aa7b75951e2a11e2c08e25bde1296c1117df51d2
Patrick Sylantys <SYLANTYS>
2022-07-28 03:13:42 -0400

    2:30 in twitter vid

--- twatter-full/pages/index.tsx
+++ twatter-full/pages/index.tsx
@@ -1,17 +1,44 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Sidebar from "../components/Sidebar"
+import Feed from "../components/Feed"
+import Widgets from "../components/Widgets"
+import { fetchTweets } from '../utils/fetchTweets'
+import { Tweet } from '../typings'
+import { Toaster } from 'react-hot-toast'
 
-const Home: NextPage = () => {
+interface Props {
+  tweets: Tweet[]
+}
+
+const Home = ({ tweets }: Props) => {
+
   return (
-    <div className="">
+    <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
       <Head>
         <title>Twitter 2.0</title>
       </Head>
+      <Toaster />
 
-      <h1>Hello World</h1>
+      <main className='grid grid-cols-9 '>
+        <Sidebar />
+
+        <Feed tweets={tweets} />
+
+        <Widgets />
+      </main>
     </div>
   )
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const tweets = await fetchTweets();
+  
+  return {
+    props: {
+      tweets,
+    }
+  }
+}
